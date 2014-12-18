0968d9ab1fad1c633cdf73ca175d5761d3767e5d
HelloCodeMing <huanmingwong@163.com>
2014-12-18 21:03:59 +0800

    first version, judge depends on last-modified and request interval is static

--- src/index.js
+++ src/index.js
@@ -1,14 +1,61 @@
 var request = require("request");
 
-function bootstrap(sites, options) {
-	
+var sitesQueue = [];
+var robotConfig = {
+	interval : 10 * 1000
+};
+
+function bootstrap(sites, options, cb) {
+	var now = new Date();
+	sites.forEach(function(url) { 
+		sitesQueue.push(new Site(url, now));
+	});
+	run(sitesQueue, cb);
 }
 
 
-function run(cb) {
-
+function run(queue, cb) {
+	var interval = setInterval(function() {
+		queue.forEach(function (site) {
+			fetch(site, cb);
+		});
+	}, robotConfig.interval);
 }
+
+/**
+ * @desc: query a site use head method, if status code is 200, invoke the callback.
+ */
+
+function fetch(site, cb) {
+	request.head(site, function(err, res) {
+		if(!err && res.statusCode == 200) {
+			site.headers['If-Modified-Since'] = res.headers['last-modified'] ||
+												res.headers['Last-Modified'] || "";
+			console.log(res.statusCode, site.headers['If-Modified-Since'], site.url);
+			cb(site.url);
+		} else {
+			console.log(res.statusCode + ":\t" + site.url);
+		}
+	});
+}
+
+/**
+ * @desc: each Site object has a url and last modified time, to judge if the site has modified.
+ */
+
+function Site(url, lastModified) {
+	this.url = url;
+	this.headers = {
+		'If-Modified-Since': lastModified
+	};
+}
+
 exports.watch = function(sites, options, cb) {
-	bootstrap(sites, options);
-	run(cb);
+	if (arguments.length == 3) {
+		bootstrap(sites, options, cb);
+	} else if (arguments.length == 2) {
+		bootstrap(sites, {}, options);
+	} else {
+		bootstrap(sites, {}, function(){});
+	}
 }
--- test/test.js
+++ test/test.js
@@ -1,7 +1,11 @@
 var watcher = require("../src")
-var sites = ["http://wanghuanming.com", "http://coolshell.cn", "http://ruanyifeng.com"]
+var sites = ["http://wanghuanming.com", 
+			"http://coolshell.cn", 
+			"http://ruanyifeng.com",
+			"http://baidu.com",
+			"http://mail.163.com",
+			"http://github.com"]
 
 
 watcher.watch(sites, function(site) {
-	console.log("updated:\t" + site);
 });
